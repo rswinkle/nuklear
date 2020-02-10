38c413f7365d8fe0f96011ae772dbbed4c6a5514
Robert Winkler <rob121618@gmail.com>
2020-02-09 21:55:51 -0700

    Finish tab

--- demo/style_configurator.c
+++ demo/style_configurator.c
@@ -2078,6 +2078,118 @@ style_combo(struct nk_context* ctx, struct nk_style_combo* out_style)
 	*out_style = combo;
 }
 
+static void
+style_tab(struct nk_context* ctx, struct nk_style_tab* out_style)
+{
+	struct nk_style_tab tab = *out_style;
+	//tab = &style->tab;
+	//tab->background         = nk_style_item_color(table[NK_COLOR_TAB_HEADER]);
+	//tab->border_color       = table[NK_COLOR_BORDER];
+	//tab->text               = table[NK_COLOR_TEXT];
+	//tab->sym_minimize       = NK_SYMBOL_TRIANGLE_RIGHT;
+	//tab->sym_maximize       = NK_SYMBOL_TRIANGLE_DOWN;
+	//tab->padding            = nk_vec2(4,4);
+	//tab->spacing            = nk_vec2(4,4);
+	//tab->indent             = 10.0f;
+	//tab->border             = 1;
+	//tab->rounding           = 0;
+
+	char buffer[64];
+
+	nk_layout_row_dynamic(ctx, 30, 2);
+
+	nk_label(ctx, "Background:", NK_TEXT_LEFT);
+	if (nk_combo_begin_color(ctx, tab.background.data.color, nk_vec2(nk_widget_width(ctx), 400))) {
+		nk_layout_row_dynamic(ctx, 120, 1);
+		struct nk_colorf colorf = nk_color_picker(ctx, nk_color_cf(tab.background.data.color), NK_RGB);
+		nk_layout_row_dynamic(ctx, 25, 1);
+		colorf.r = nk_propertyf(ctx, "#R:", 0, colorf.r, 1.0f, 0.01f,0.005f);
+		colorf.g = nk_propertyf(ctx, "#G:", 0, colorf.g, 1.0f, 0.01f,0.005f);
+		colorf.b = nk_propertyf(ctx, "#B:", 0, colorf.b, 1.0f, 0.01f,0.005f);
+
+		tab.background.data.color = nk_rgb_cf(colorf);
+
+		nk_combo_end(ctx);
+	}
+
+	nk_label(ctx, "Border Color:", NK_TEXT_LEFT);
+	if (nk_combo_begin_color(ctx, tab.border_color, nk_vec2(nk_widget_width(ctx), 400))) {
+		nk_layout_row_dynamic(ctx, 120, 1);
+		struct nk_colorf colorf = nk_color_picker(ctx, nk_color_cf(tab.border_color), NK_RGB);
+		nk_layout_row_dynamic(ctx, 25, 1);
+		colorf.r = nk_propertyf(ctx, "#R:", 0, colorf.r, 1.0f, 0.01f,0.005f);
+		colorf.g = nk_propertyf(ctx, "#G:", 0, colorf.g, 1.0f, 0.01f,0.005f);
+		colorf.b = nk_propertyf(ctx, "#B:", 0, colorf.b, 1.0f, 0.01f,0.005f);
+
+		tab.border_color = nk_rgb_cf(colorf);
+
+		nk_combo_end(ctx);
+	}
+
+	nk_label(ctx, "Text:", NK_TEXT_LEFT);
+	if (nk_combo_begin_color(ctx, tab.text, nk_vec2(nk_widget_width(ctx), 400))) {
+		nk_layout_row_dynamic(ctx, 120, 1);
+		struct nk_colorf colorf = nk_color_picker(ctx, nk_color_cf(tab.text), NK_RGB);
+		nk_layout_row_dynamic(ctx, 25, 1);
+		colorf.r = nk_propertyf(ctx, "#R:", 0, colorf.r, 1.0f, 0.01f,0.005f);
+		colorf.g = nk_propertyf(ctx, "#G:", 0, colorf.g, 1.0f, 0.01f,0.005f);
+		colorf.b = nk_propertyf(ctx, "#B:", 0, colorf.b, 1.0f, 0.01f,0.005f);
+
+		tab.text = nk_rgb_cf(colorf);
+
+		nk_combo_end(ctx);
+	}
+
+	// putting these in matching order instead of at bottom because we have an odd number of float properties
+	const char* symbols[NK_SYMBOL_MAX] =
+{
+    "NONE",
+    "X",
+    "UNDERSCORE",
+    "CIRCLE_SOLID",
+    "CIRCLE_OUTLINE",
+    "RECT_SOLID",
+    "RECT_OUTLINE",
+    "TRIANGLE_UP",
+    "TRIANGLE_DOWN",
+    "TRIANGLE_LEFT",
+    "TRIANGLE_RIGHT",
+    "PLUS",
+    "MINUS"
+};
+
+	nk_label(ctx, "Minimize Symbol:", NK_TEXT_LEFT);
+	tab.sym_minimize = nk_combo(ctx, symbols, NK_SYMBOL_MAX, tab.sym_minimize, 25, nk_vec2(200,200));
+	nk_label(ctx, "Maxmize Symbol:", NK_TEXT_LEFT);
+	tab.sym_maximize = nk_combo(ctx, symbols, NK_SYMBOL_MAX, tab.sym_maximize, 25, nk_vec2(200,200));
+
+	nk_label(ctx, "Padding:", NK_TEXT_LEFT);
+	sprintf(buffer, "%.2f, %.2f", tab.padding.x, tab.padding.y);
+	if (nk_combo_begin_label(ctx, buffer, nk_vec2(200,200))) {
+		nk_layout_row_dynamic(ctx, 25, 1);
+		nk_property_float(ctx, "#X:", -100.0f, &tab.padding.x, 100.0f, 1,0.5f);
+		nk_property_float(ctx, "#Y:", -100.0f, &tab.padding.y, 100.0f, 1,0.5f);
+		nk_combo_end(ctx);
+	}
+
+	nk_label(ctx, "Padding:", NK_TEXT_LEFT);
+	sprintf(buffer, "%.2f, %.2f", tab.spacing.x, tab.spacing.y);
+	if (nk_combo_begin_label(ctx, buffer, nk_vec2(200,200))) {
+		nk_layout_row_dynamic(ctx, 25, 1);
+		nk_property_float(ctx, "#X:", -100.0f, &tab.spacing.x, 100.0f, 1,0.5f);
+		nk_property_float(ctx, "#Y:", -100.0f, &tab.spacing.y, 100.0f, 1,0.5f);
+		nk_combo_end(ctx);
+	}
+
+	nk_property_float(ctx, "#Indent:", -100.0f, &tab.indent, 100.0f, 1,0.5f);
+	nk_property_float(ctx, "#Border:", -100.0f, &tab.border, 100.0f, 1,0.5f);
+	nk_property_float(ctx, "#Rounding:", -100.0f, &tab.rounding, 100.0f, 1,0.5f);
+
+
+
+	*out_style = tab;
+}
+
 static int
 style_configurator(struct nk_context *ctx)
 {
@@ -2224,6 +2336,11 @@ style_configurator(struct nk_context *ctx)
 			nk_tree_pop(ctx);
 		}
 
+		if (nk_tree_push(ctx, NK_TREE_TAB, "Tab", NK_MINIMIZED)) {
+			style_tab(ctx, &style->tab);
+			nk_tree_pop(ctx);
+		}
+
 
 		nk_layout_row_dynamic(ctx, 30, 1);
 		if (nk_button_label(ctx, "Reset all styles to defaults")) {
@@ -2232,19 +2349,6 @@ style_configurator(struct nk_context *ctx)
 
 	}
 
-//
-//		/* tab */
-//		tab = &style->tab;
-//		tab->background         = nk_style_item_color(table[NK_COLOR_TAB_HEADER]);
-//		tab->border_color       = table[NK_COLOR_BORDER];
-//		tab->text               = table[NK_COLOR_TEXT];
-//		tab->sym_minimize       = NK_SYMBOL_TRIANGLE_RIGHT;
-//		tab->sym_maximize       = NK_SYMBOL_TRIANGLE_DOWN;
-//		tab->padding            = nk_vec2(4,4);
-//		tab->spacing            = nk_vec2(4,4);
-//		tab->indent             = 10.0f;
-//		tab->border             = 1;
-//		tab->rounding           = 0;
 //
 //		/* window header */
 //		win = &style->window;
